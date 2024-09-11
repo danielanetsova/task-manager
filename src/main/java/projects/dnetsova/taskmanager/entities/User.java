1c42984da53e007a38ec5085448d88391f06b5f1
Daniela Netsova <netsovadaniela@gmail.com>
2024-09-11 12:59:40 +0300

    Moddified UserRepository and User
    Added three methods in UserRepository.
    Added constructor and a getter method for the field 'name' in User.

--- src/main/java/projects/dnetsova/taskmanager/entities/User.java
+++ src/main/java/projects/dnetsova/taskmanager/entities/User.java
@@ -15,6 +15,16 @@ public class User {
     @Column(unique = true, nullable = false)
     private String name;
 
+    public User() {}
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/projects/dnetsova/taskmanager/repositories/UserRepository.java
+++ src/main/java/projects/dnetsova/taskmanager/repositories/UserRepository.java
@@ -1,6 +1,11 @@
 package projects.dnetsova.taskmanager.repositories;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import projects.dnetsova.taskmanager.entities.User;
 
@@ -8,4 +13,17 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.name = :newName WHERE u.name = :name")
+    int updateUserName(String name, String newName);
+
+    @Query("SELECT u.name FROM User u")
+    Page<String> findAllUserNames(Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM User u WHERE u.name = :name")
+    int delete(String name);
 }
